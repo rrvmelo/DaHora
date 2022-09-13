62445b7928f7e11f548801883fd9a23535e951e8
rrvmelo <rrvmelo@gmail.com>
2022-09-13 08:55:50 -0300

    Commit-Inicial

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'user',
+    password: 'Mininova.Org..',
+    database: 'companysql',
+    define: {
+        timestamps: true,
+    },
+};
