e4f9021335c8d524acd5f89e0524a2f4c9fdb3f0
rrvmelo <rrvmelo@gmail.com>
2022-11-18 18:37:17 -0300

    Update para Deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'admin',
+    host: 'us-cdbr-east-06.cleardb.net', //localhost
+    username: 'be5d270c2d58c8', //root
+    password: '45e39ad2', //admin
     timezone: '-03:00',
-    database: 'dahora',
+    database: 'heroku_ddec2fb71205c8a', //dahora
     define: {
         timestamps: true,
     },
